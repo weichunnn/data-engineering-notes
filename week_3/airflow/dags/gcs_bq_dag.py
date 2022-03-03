c09e4dc74e4cb5de6eb8d77f9cc2109edc26bfb6
Wei Chun <tanweichun@outlook.com>
2022-03-03 04:55:17 +0000

    adapt for multi color

--- week_3/airflow/dags/gcs_bq_dag.py
+++ week_3/airflow/dags/gcs_bq_dag.py
@@ -7,7 +7,6 @@ from airflow.providers.google.cloud.operators.bigquery import (
     BigQueryCreateExternalTableOperator,
     BigQueryInsertJobOperator,
 )
-
 from airflow.providers.google.cloud.transfers.gcs_to_gcs import GCSToGCSOperator
 
 PROJECT_ID = os.environ.get("GCP_PROJECT_ID")
@@ -16,6 +15,10 @@ BUCKET = os.environ.get("GCP_GCS_BUCKET")
 path_to_local_home = os.environ.get("AIRFLOW_HOME", "/opt/airflow/")
 BIGQUERY_DATASET = os.environ.get("BIGQUERY_DATASET", "trips_data_all")
 
+DATASET = "tripdata"
+COLOUR_RANGE = {"yellow": "tpep_pickup_datetime", "green": "lpep_pickup_datetime"}
+INPUT_PART = "raw"
+INPUT_FILETYPE = "parquet"
 
 default_args = {
     "owner": "airflow",
@@ -24,43 +27,54 @@ default_args = {
     "retries": 1,
 }
 
-
 with DAG(
-    dag_id="gcs_bq_dag",
+    dag_id="gcs_2_bq_dag",
     schedule_interval="@daily",
-    catchup=False,
-    tags=["dtc-de"],
-    max_active_runs=1,
     default_args=default_args,
+    catchup=False,
+    max_active_runs=1,
+    tags=["dtc-de"],
 ) as dag:
-    gcs_to_gcs_task = GCSToGCSOperator(
-        task_id="gcs_to_gcs_task",
-        source_bucket=BUCKET,
-        source_objects=["raw/yellow_tripdata/2019/*.parquet", "raw/yellow_tripdata/2020/*.parquet"],
-        destination_bucket=BUCKET,
-        destination_object="yellow/",
-    )
+    for colour, ds_col in COLOUR_RANGE.items():
+        move_files_gcs_task = GCSToGCSOperator(
+            task_id=f"move_{colour}_{DATASET}_files_task",
+            source_bucket=BUCKET,
+            source_object=f"{INPUT_PART}/{colour}_{DATASET}*.{INPUT_FILETYPE}",
+            destination_bucket=BUCKET,
+            destination_object=f"{colour}",
+        )
 
-    gcs_to_bq_ext_task = BigQueryCreateExternalTableOperator(
-        task_id="gcs_to_bq_ext_task",
-        table_resource={
-            "tableReference": {
-                "projectId": PROJECT_ID,
-                "datasetId": BIGQUERY_DATASET,
-                "tableId": "external_yellow_tripdata",
+        bigquery_external_table_task = BigQueryCreateExternalTableOperator(
+            task_id=f"bq_{colour}_{DATASET}_external_table_task",
+            table_resource={
+                "tableReference": {
+                    "projectId": PROJECT_ID,
+                    "datasetId": BIGQUERY_DATASET,
+                    "tableId": f"{colour}_{DATASET}_external_table",
+                },
+                "externalDataConfiguration": {
+                    "autodetect": "True",
+                    "sourceFormat": f"{INPUT_FILETYPE.upper()}",
+                    "sourceUris": [f"gs://{BUCKET}/{colour}/*"],
+                },
             },
-            "externalDataConfiguration": {"sourceFormat": "PARQUET", "sourceUris": [f"gs://{BUCKET}/yellow/*"],},
-        },
-    )
+        )
 
-    CREATE_PART_TBL_QUERY = f"CREATE OR REPLACE TABLE {BIGQUERY_DATASET}.yellow_tripdata_partitoned \
-        PARTITION BY DATE(tpep_pickup_datetime) AS \
-        SELECT * FROM {BIGQUERY_DATASET}.external_yellow_tripdata"
+        CREATE_BQ_TBL_QUERY = f"CREATE OR REPLACE TABLE {BIGQUERY_DATASET}.{colour}_{DATASET} \
+            PARTITION BY DATE({ds_col}) \
+            AS \
+            SELECT * FROM {BIGQUERY_DATASET}.{colour}_{DATASET}_external_table;"
 
-    bg_ext_to_part_task = BigQueryInsertJobOperator(
-        task_id="bg_ext_to_part_task",
-        configuration={"query": {"query": CREATE_PART_TBL_QUERY, "useLegacySql": False,},},
-    )
+        if colour == "green":
+            CREATE_BQ_TBL_QUERY = f"CREATE OR REPLACE TABLE {BIGQUERY_DATASET}.{colour}_{DATASET} \
+                PARTITION BY DATE({ds_col}) \
+                AS \
+                SELECT * EXCEPT (ehail_fee) FROM {BIGQUERY_DATASET}.{colour}_{DATASET}_external_table;"
 
-    gcs_to_gcs_task
+        # Create a partitioned table from external table
+        bq_create_partitioned_table_job = BigQueryInsertJobOperator(
+            task_id=f"bq_create_{colour}_{DATASET}_partitioned_table_task",
+            configuration={"query": {"query": CREATE_BQ_TBL_QUERY, "useLegacySql": False,}},
+        )
 
+        move_files_gcs_task >> bigquery_external_table_task >> bq_create_partitioned_table_job
